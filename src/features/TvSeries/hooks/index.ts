4c039215df79273873715b9468743bfc1ca4df6a
reyulinnaja <aldilla.ulinnaja@rey.id>
2023-09-15 10:22:53 +0700

    feat(tvseries): develop tvseriespage Sort, Filter, InfiniteScroll

---
+++ src/features/TvSeries/hooks/index.ts
@@ -0,0 +1,24 @@
+import { useNavbarStore } from "@/hooks/useNavbarStore";
+import { shallow } from "zustand/shallow";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { axiosInstance } from "@/lib/axios";
+
+export const useDiscoverTvSeriesQuery = () => {
+  const [sortName, sortBy, includeAdult] = useNavbarStore(
+    (state) => [state.sortName, state.sortBy, state.includeAdult],
+    shallow,
+  );
+
+  return useInfiniteQuery(
+    ["discover_tv", sortName, sortBy],
+    async ({ pageParam = 1 }) => {
+      const response = await axiosInstance.get(
+        `/discover/tv?page=${pageParam}&sort_by=${sortName}.${sortBy}&include_adult=${includeAdult}`,
+      );
+      return response.data;
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.page + 1,
+    },
+  );
+};
--- src/features/TvSeries/index.tsx
+++ src/features/TvSeries/index.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { BaseHeader, Card } from "@/components/common";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useDiscoverTvSeriesQuery } from "./hooks";
 
 const TvSeries = () => {
-  return <div>TvSeries</div>;
+  const { data, fetchNextPage, hasNextPage, isLoading } =
+    useDiscoverTvSeriesQuery();
+
+  return (
+    <div>
+      <BaseHeader title="Tv Series - ReyMovies" />
+      <h1 className="text-2xl font-bold">Discover Tv Series</h1>
+      <div>
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          data && (
+            <InfiniteScroll
+              dataLength={data.pages.length}
+              next={fetchNextPage}
+              hasMore={hasNextPage || false}
+              loader={<h4>Loading...</h4>}
+              endMessage={<p>End of list</p>}
+            >
+              {data.pages.map((pageData, i) => (
+                <div key={i} className="mt-3 grid grid-cols-5 gap-x-4 gap-y-3">
+                  {pageData.results.map((item: any) => (
+                    <Card
+                      key={item.id}
+                      href={`/tvseries/${item.id}`}
+                      {...item}
+                    />
+                  ))}
+                </div>
+              ))}
+            </InfiniteScroll>
+          )
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default TvSeries;
