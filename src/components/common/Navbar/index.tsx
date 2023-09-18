21251b5b7cdbe343a307ff17942b3ae3a4aaa19d
reyulinnaja <aldilla.ulinnaja@rey.id>
2023-09-18 16:20:38 +0700

    feat: develop login function

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -28,6 +30,9 @@ import {
 } from "@/components/ui/select";
 import { Checkbox } from "@/components/ui/checkbox";
 import { debounce } from "@/lib/utils";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "@/lib/axios";
+import { BiChevronDown } from "react-icons/bi";
 
 const Navbar = () => {
   const router = useRouter();
@@ -71,6 +76,41 @@ const Navbar = () => {
     const newQuery = e.target.value;
     setQuery(newQuery);
   }, 500);
+
+  const { mutate: login } = useMutation(
+    async () => {
+      const response = await axiosInstance.get("/authentication/token/new");
+      return response.data;
+    },
+    {
+      onSuccess: (data) => {
+        localStorage.setItem("token", data.request_token);
+        window.location.href = `https://www.themoviedb.org/authenticate/${data.request_token}?redirect_to=http://localhost:3000`;
+      },
+    },
+  );
+
+  const { mutate: getSession } = useMutation(
+    async () => {
+      const response = await axiosInstance.post("/authentication/session/new", {
+        request_token: localStorage.getItem("token"),
+      });
+
+      console.log(response.data);
+      return response.data;
+    },
+    {
+      onSuccess: (data) => {
+        localStorage.setItem("session_id", data.session_id);
+      },
+    },
+  );
+
+  useEffect(() => {
+    if (window.location.href.includes("approved=true")) {
+      getSession();
+    }
+  }, []);
 
   return (
     <nav
@@ -209,13 +249,22 @@ const Navbar = () => {
       </TooltipProvider>
 
       {/* Avatar */}
-      <Avatar className="bg-slate-700">
-        <AvatarImage
-          src="https://api.dicebear.com/7.x/lorelei/svg?seed=U"
-          alt="Ulinnaja"
-        />
-        <AvatarFallback>UN</AvatarFallback>
-      </Avatar>
+      {/* <div className="flex items-center gap-3 rounded-full bg-slate-800">
+          <Avatar className="bg-slate-700">
+            <AvatarImage
+              src="https://api.dicebear.com/7.x/lorelei/svg?seed=U"
+              alt="Ulinnaja"
+            />
+            <AvatarFallback>UN</AvatarFallback>
+          </Avatar>
+          <div className="flex items-center gap-1 pr-2">
+            <p>Ulinnaja</p>
+            <BiChevronDown className="h-5 w-5" />
+          </div>
+        </div> */}
+      <Button variant="outline" onClick={() => login()}>
+        Login
+      </Button>
     </nav>
   );
 };
